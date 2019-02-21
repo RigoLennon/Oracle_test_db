7db6b83e9628af5ff5df14eae71cbb3a5d67acc5
Rigoberto Alcantar Aguilar <a20150338@utem.edu.mx>
2019-02-20 18:00:31 -0600

    modfi

--- Oracle_test_db.sql
+++ Oracle_test_db.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE TEST_GIT AS
+CREATE OR REPLACE PROCEDURE TEST_GIT_x2 AS
 BEGIN
   dbms_output.put_line('Probando SQL Developer con Git');
   dbms_output.put_line('Hola mundo');
